c7d10604a8ba1655fd867ab08f538527ab3a6f25
Suman Saurabh <sumanan047@gmail.com>
2025-01-12 20:14:52 -0600

    Add initial conditions and plot for debugging

--- dev_script.py
+++ dev_script.py
@@ -1,7 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from nietzsche.utils import Dimension
 from nietzsche.time_ import Time
 from nietzsche.space import Space
-from nietzsche.vib_ode import Vibration
+from nietzsche.vibration import Vibration
+from nietzsche.initial_condition import InitialConditions
 
 # set time
 t = Time(start = 0.0, step = 100, dt = 0.1)
@@ -12,4 +16,14 @@ s = Space(dimension=Dimension.DD.value, x_start=0, x_stop=1, x_step=25)
 # set vibration
 v = Vibration(time=t, space=s)
 
-print(v.primal_domain.shape)
+# set initial conditions
+def quad_func(x):
+    return 34*np.sin(x)*np.cos(x)
+
+ic = InitialConditions(domain=np.linspace(0, 1, 10), mode="function", value=10, funcion=quad_func)
+ic.set()
+
+
+# plot to debug
+plt.plot(ic.domain,ic.ic_arr)
+plt.show()
